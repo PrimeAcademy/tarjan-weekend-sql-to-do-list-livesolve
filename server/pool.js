f2992e02de6332e0c1270eb708c05ac47dfe6f50
Edan Schwartz <edanschwartz@gmail.com>
2020-09-14 16:34:22 -0500

    Configure DB for heroku

--- server/pool.js
+++ server/pool.js
@@ -2,8 +2,12 @@ const pg = require('pg');
 
 console.log(`What's my database URL?`, process.env.DATABASE_URL);
 
+// const pool = new pg.Pool({
+//   database: 'weekend-to-do-app',
+// });
+
 const pool = new pg.Pool({
-  database: 'weekend-to-do-app',
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
