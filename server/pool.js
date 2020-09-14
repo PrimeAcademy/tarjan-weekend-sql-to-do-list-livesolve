fb3305d61c020761baae8711e4ff9b157a094eac
Edan Schwartz <edanschwartz@gmail.com>
2020-09-14 16:52:33 -0500

    Update pg config for local

--- server/pool.js
+++ server/pool.js
@@ -6,8 +6,18 @@ console.log(`What's my database URL?`, process.env.DATABASE_URL);
 //   database: 'weekend-to-do-app',
 // });
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
-});
+let pool;
+if (process.env.DATABASE_URL) {
+  console.log("Gonna connect to a heroku DB");
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+  });
+}
+else {
+  console.log("Assuming we're running locally");
+  pool = new pg.Pool({
+    database: "weekend-to-do-app"
+  });
+}
 
 module.exports = pool;
