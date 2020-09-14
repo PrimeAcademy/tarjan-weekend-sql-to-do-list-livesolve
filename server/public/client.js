e8766ec7510b028a0ce6d69343f113e92ce0a217
Edan Schwartz <edanschwartz@gmail.com>
2020-09-14 10:47:44 -0500

    Render tasks to DOM

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,34 @@
 $(document).ready(onReady);
 
 function onReady() {
-  console.log("So ready.")
+  console.log("So ready.");
+
+  // Render tasks on page load
+  renderTasks();
+}
+
+/**
+ * Gets tasks from server (GET /tasks)
+ * and renders them to the DOM
+ */
+function renderTasks() {
+  $.ajax({
+    method: "GET",
+    url: "/tasks"
+  }).then(response => {
+    console.log("Got a response from GET /tasks", response);
+
+    // Look through each todo item
+    for (let task of response) {
+      $('#todoList').append(`
+        <li>
+          <input type="checkbox" />
+          ${task.name}
+          <button>Delete</button>
+        </li>
+      `)
+    }
+  }).catch(err => {
+    console.error("GET /tasks failed", err);
+  });
 }
