f45713e120eb9e1334c0a582053deeeff3d5346d
Edan Schwartz <edanschwartz@gmail.com>
2020-09-14 10:24:10 -0500

    Setup uses

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// Serve static files from server/public folder
+app.use(express.static('server/public'));
+
+// Setup body parser to read request JSON body
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
 app.listen(port, () => {
